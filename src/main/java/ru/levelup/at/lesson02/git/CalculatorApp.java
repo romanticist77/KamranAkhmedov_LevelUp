bdb8ea3e0685261b8946352c6594a4b5cadf25e9
romanticist77 <romanticist77@gmail.com>
2023-05-20 12:45:27 +0300

    added a CalculatorAp draft

--- src/main/java/ru/levelup/at/lesson02/git/CalculatorApp.java
+++ src/main/java/ru/levelup/at/lesson02/git/CalculatorApp.java
@@ -1,6 +1,6 @@
 package ru.levelup.at.lesson02.git;
 
-public class Calculator {
+public class CalculatorApp {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
