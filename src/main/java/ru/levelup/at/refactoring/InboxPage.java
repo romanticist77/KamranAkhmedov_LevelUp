d4e8513a5a6e37e88276f74a3685c7e60dc48f8d
romanticist77 <romanticist77@gmail.com>
2023-06-13 23:15:52 +0300

    finished refactoring customRulesTest

--- src/main/java/ru/levelup/at/refactoring/InboxPage.java
+++ src/main/java/ru/levelup/at/refactoring/InboxPage.java
@@ -8,70 +8,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class InboxPage extends MailBasePage {
 
-    //        var incomingMessagesButton =
-    //            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@data-folder-link-id='0']")));
-    //
-    //        assertThat(incomingMessagesButton.getText()).as("Нет ожидаемой вкладки на странице").contains("Входящие");
-    //
-    //        var writeLetterButton =
-    //            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@data-title-shortcut='N']")));
-    //        writeLetterButton.click();
-    //
-    //        var recipient = wait.until(
-    //            ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='input--3slxg']//div//input")));
-    //        recipient.sendKeys("test94.00@mail.ru");
-    //        var subject = wait.until(ExpectedConditions.visibilityOfElementLocated(
-    //            By.xpath("//div[@class='subject__container--HWnat']//div//input")));
-    //        subject.sendKeys(uuidAsString);
-    //        var body = wait.until(ExpectedConditions.visibilityOfElementLocated(
-    //            By.xpath("//div[contains(@class, 'editable-container-')]/div[contains(@class, 'editable-')]")));
-    //        body.sendKeys("Hello, this is a testing letter.");
-    //
-    //        var saveButton =
-    //            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@data-test-id='save']")));
-    //        saveButton.click();
-    //
-    //        var lookDraftsButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
-    //            "//span[@class='notify__Посмотреть']")));
-    //        lookDraftsButton.click();
-
-    //        var verifySubject = wait.until(ExpectedConditions.visibilityOfElementLocated(
-    //            By.xpath("//span[contains(text(),'%s')]".formatted(uuidAsString))));
-    //        System.out.println(
-    //            "Drafts. Target subject is found: " + verifySubject);
-    //        verifySubject.click();
-    //
-    //        var sendButton =
-    //            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@data-test-id='send']")));
-    //        sendButton.click();
-    //
-    //        var closeModalWindowButton =
-    //            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='layer__controls']/span")));
-    //        closeModalWindowButton.click();
-    //
-    //        var verifyMessageSent =
-    //            wait.until(ExpectedConditions.invisibilityOfElementLocated(
-    //                By.xpath("//span[text()='%s']".formatted(uuidAsString))));
-    //
-    //        System.out.println("Drafts. Target subject is not found: " + verifyMessageSent);
-    //
-    //        var sentMessagesButton =
-    //            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Отправленные']")));
-    //        sentMessagesButton.click();
-    //
-    //        verifySubject = wait.until(ExpectedConditions.visibilityOfElementLocated(
-    //            By.xpath("//span[text()='Self: %s']".formatted(uuidAsString))));
-    //
-    //        System.out.println(
-    //            "Sent. Target subject is found: " + verifySubject);
-    //
-    //        var profileButton = wait.until(
-    //            ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[contains(@class,'ph-avatar-img')]")));
-    //        profileButton.click();
-    //
-    //        var exitButton = wait.until((ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Выйти']"))));
-    //        exitButton.click();
-
     @FindBy(xpath = "//a[@data-folder-link-id='0']")
     private WebElement incomingMessagesButton;
     @FindBy(xpath = "//a[@data-title-shortcut='N']")
@@ -88,24 +24,25 @@ public class InboxPage extends MailBasePage {
     private WebElement lookDraftsButton;
     @FindBy(xpath = "ll-sj__normal")
     private WebElement verifySubject;
+    @FindBy(xpath = "//span[contains(@title,'test94.00@mail.ru')]")
+    private WebElement filledRecipient;
+    @FindBy(xpath = "//div[text()='Hello, this is a testing letter.']")
+    private WebElement filledBody;
     @FindBy(xpath = "//button[@data-test-id='send']")
     private WebElement sendButton;
     @FindBy(xpath = "//div[@class='layer__controls']/span")
     private WebElement closeModalWindowButton;
-    @FindBy(xpath = "//span[text()='%s']")
-    private WebElement verifyMessageSent;
-
     @FindBy(xpath = "//div[text()='Отправленные']")
     private WebElement sentMessagesButton;
-    @FindBy(xpath = "//span[text()='Self: %s']")
-    private WebElement verifySelfSubject;
     @FindBy(xpath = "//img[contains(@class,'ph-avatar-img')]")
     private WebElement profileButton;
     @FindBy(xpath = "//div[text()='Выйти']")
     private WebElement exitButton;
+    @FindBy(xpath = "//div[text()= 'Тест']/parent::div/parent::div")
+    private WebElement testFolder;
 
-    public void clickIncomingMessagesButton() {
-        wait.until(ExpectedConditions.visibilityOf(incomingMessagesButton)).click();
+    public void clickTestMessagesButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(testFolder)).click();
     }
 
     public WebElement getIncomingMessagesButton() {
@@ -122,6 +59,10 @@ public class InboxPage extends MailBasePage {
 
     public void fillSubject() {
         wait.until(ExpectedConditions.visibilityOf(subject)).sendKeys(uuidAsString);
+    }
+
+    public void fillTestSubject() {
+        wait.until(ExpectedConditions.visibilityOf(subject)).sendKeys("Test: " + uuidAsString);
     }
 
     public void fillBody() {
@@ -151,6 +92,24 @@ public class InboxPage extends MailBasePage {
             By.xpath("//span[text()='Self: %s']".formatted(uuidAsString))));
     }
 
+    public WebElement verifyTestSubjectFound() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(
+            By.xpath("//span[text()='Test: %s']".formatted(uuidAsString))));
+    }
+
+    public WebElement verifySelfTestSubjectFound() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(
+            By.xpath("//span[text()='Self: Test: %s']".formatted(uuidAsString))));
+    }
+
+    public WebElement verifyRecipientFound() {
+        return wait.until(ExpectedConditions.elementToBeClickable(filledRecipient));
+    }
+
+    public WebElement verifyBodyFound() {
+        return wait.until(ExpectedConditions.elementToBeClickable(filledBody));
+    }
+
     public void clickOnSubject() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(
             By.xpath("//span[contains(text(),'%s')]".formatted(uuidAsString)))).click();
@@ -163,7 +122,6 @@ public class InboxPage extends MailBasePage {
     public void clickCloseModalWindowButton() {
         wait.until(ExpectedConditions.elementToBeClickable(closeModalWindowButton)).click();
     }
-    // verifySubject sendButton closeModalWindowButton  verifyMessageSent sentMessagesButton verifySubject
 
     public void clickSentMessagesButton() {
         wait.until(ExpectedConditions.elementToBeClickable(sentMessagesButton)).click();
